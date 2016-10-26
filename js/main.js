689e46ff893772124cfe21c9a359883e67bac192
Jeff Auriemma <jdauriemma@gmail.com>
2016-10-26 17:19:18 -0400

    Add overlay and restart script

--- js/main.js
+++ js/main.js
@@ -2,7 +2,28 @@
   'use strict';
 
   function init () {
-    console.log('hi');
+    var $restart = document.querySelector('#restart');
+    var $overlay = document.querySelector('#overlay');
+    var $cancel = document.querySelector('#cancel');
+    var $confirm = document.querySelector('#confirm');
+
+    function restart () {
+      $overlay.style.display = 'block';
+    }
+
+    function cancel () {
+      $overlay.style.display = 'none';
+    }
+
+    function confirmRestart () {
+      window.location.href = window.location.href.replace(/\/\d(\.html)?/, '');
+    }
+
+    $restart.addEventListener('click', restart);
+
+    $cancel.addEventListener('click', cancel);
+
+    $confirm.addEventListener('click', confirmRestart);
   }
 
   document.addEventListener('DOMContentLoaded', init);
